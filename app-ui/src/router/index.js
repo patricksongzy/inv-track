3b21b7e1ea3c4f20c1aa5ed9d6063f8948ecba3f
Patrick Song <patricksongzy@gmail.com>
2022-01-17 11:26:18 -0500

    Use web hash history

--- app-ui/src/router/index.js
+++ app-ui/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import ItemsPage from '../views/ItemsPage.vue';
 import ItemPage from '../views/ItemPage.vue';
@@ -18,7 +18,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
